cedc7d97ecab27eac2036a90086086e01853e875
SANKET KAPILE <sanketkapile1090@gmail.com>
2024-07-21 21:43:53 +0100

    adding deposite and view account feature

--- src/main/java/com/sanket/banking/service/AccountService.java
+++ src/main/java/com/sanket/banking/service/AccountService.java
@@ -5,5 +5,14 @@ import com.sanket.banking.dto.AccountDto;
 
 public interface AccountService {
 
+	//Create New Account
 	AccountDto createAccount(AccountDto accountDto);
+	
+	//Display Account Information
+	AccountDto getAccountById(Long id);
+	
+	//Deposit balance
+	AccountDto deposit(Long id, double amount);
+	
+	
 }
--- src/main/java/com/sanket/banking/service/impl/AccountServiceImpl.java
+++ src/main/java/com/sanket/banking/service/impl/AccountServiceImpl.java
@@ -9,11 +9,10 @@ import com.sanket.banking.repository.AccountRepository;
 import com.sanket.banking.service.AccountService;
 
 @Service
-public class AccountServiceImpl implements AccountService{
+public class AccountServiceImpl implements AccountService {
 
 	private AccountRepository accountRepository;
-	
-	
+
 	public AccountServiceImpl(AccountRepository accountRepository) {
 		super();
 		this.accountRepository = accountRepository;
@@ -24,6 +23,26 @@ public class AccountServiceImpl implements AccountService{
 		Account account = AccountMapper.mapToAccount(accountDto);
 		Account savedAccount = accountRepository.save(account);
 		return AccountMapper.mapToAccountDto(savedAccount);
+	}
+
+	@Override
+	public AccountDto getAccountById(Long id) {
+		Account account = accountRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Account does not exists"));
+
+		return AccountMapper.mapToAccountDto(account);
+	}
+
+	@Override
+	public AccountDto deposit(Long id, double amount) {
+		Account account = accountRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Account does not exists"));
+
+		double total = account.getBalance() + amount;
+		account.setBalance(total);
+		Account savedAccount = accountRepository.save(account);
+		return AccountMapper.mapToAccountDto(savedAccount);
+
 	}
 
 }
